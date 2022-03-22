2008968eeb1eb44915b0e988a958c7496e2802c6
SreeLakshmi118 <97936640+SreeLakshmi118@users.noreply.github.com>
2022-03-22 13:03:29 +0530

    Add files via upload

---
+++ java(22-03-2022)/Add.java
@@ -0,0 +1,8 @@
+public class Add{
+	public static void main(String args[]){
+		System.out.println("enter two integers");
+		int x=10;
+		int y=20;
+		System.out.println(x+" "+y);
+		int sum = x+y;
+System.out.println("sum is:" + sum);}}
---
+++ java(22-03-2022)/Concat.java
@@ -0,0 +1,8 @@
+public class Concat{
+	public static void main(String args[]){
+		 String s1="hai";
+		 String s2="hello";
+		String s3=s1.concat(s2);
+    System.out.println(s3);}}
+		
+		
