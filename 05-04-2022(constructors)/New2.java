f4249a761fd1f25fad2f47d24661e6f8f48f0a7b
SreeLakshmi118 <97936640+SreeLakshmi118@users.noreply.github.com>
2022-04-05 13:05:43 +0530

    Add files via upload

---
+++ 05-04-2022(constructors)/New2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class New2{
+	public static void main(String args[]){
+		String name;
+		System.out.println("Enter your Name");
+		Scanner obj = new Scanner(System.in);
+		name = obj.nextLine();
+		System.out.println("Name:"+name);
+	
+}}
